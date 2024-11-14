d0070e6e9077785f1e291017e32fa61274d49ca5
Mon200004 <154223100+Mon200004@users.noreply.github.com>
2024-11-14 09:52:14 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Load the pretrained model
-model = joblib.load('health_model.pkl')
+model = joblib.load('health_condition_model.npy')
 
 @app.route('/api/predict', methods=['POST'])
 def predict():
